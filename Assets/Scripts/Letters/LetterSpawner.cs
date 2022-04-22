63b974b5a809f2f28f5775040a49cd2c53031fda
DannyIsYog <fabiodaniel.2000@gmail.com>
2022-04-22 20:22:11 +0100

    Letter Minigame Done for real

--- Assets/Scripts/Letters/LetterSpawner.cs
+++ Assets/Scripts/Letters/LetterSpawner.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LetterSpawner : MonoBehaviour
 {
     public static LetterSpawner Instance { get; private set; }
     [SerializeField] GameObject LetterPrefab;
-
     [SerializeField] Transform spawnPoint;
+    [SerializeField] int totalLetters;
+    [SerializeField] int currentLetters = 0;
+
+    [SerializeField] int loadStory;
+
     // Start is called before the first frame update
     void Start()
     {
+        totalLetters = Random.Range(4, 10);
         SpawnLetter();
     }
 
@@ -28,6 +34,11 @@ public class LetterSpawner : MonoBehaviour
 
     public void SpawnLetter()
     {
+        if (currentLetters >= totalLetters)
+        {
+            SceneManager.LoadScene(loadStory);
+        }
+        currentLetters++;
         Instantiate(LetterPrefab, spawnPoint);
     }
 }
